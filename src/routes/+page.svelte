b7c7b2343239dad41b0d9c468c44720ca17e3848
Omarplayz233 <omarplayzsounds@gmail.com>
2024-10-22 19:06:42 +0100

    added metadata and svg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,24 @@
+<script>
+  import { page } from '$app/stores';
+
+  export let title = "Project Jam”;
+  export let description = “idk what to put here”;
+  export let image = "https://project-jam.vercel.app/logo.png”; 
+</script>
+
 <main>
     <img src="/logo.svg" alt="Logo" />
 </main>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta name="description" content={description}} | Example.com>
+  <meta property="og:url" content="https://project-jam.vercel.app/{$page.url.pathname.toString()}">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="{title}">
+  <meta property="og:description" content={description}>
+  <meta property="og:image" content={image}>
+</svelte:head>
 
 <style>
     main {
